a1a11a43a739b84ee2593c6994a4f002d9acb1f3
Samenergy <sdushime38@gmail.com>
2023-11-29 11:57:21 +0200

    updated

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-    // JavaScript to change the navbar color on scroll
-    window.onscroll = function() {
+
+      window.onscroll = function() {
         var navbar = document.getElementById('navbar');
+        var links = document.querySelectorAll('#navbar-section a');
   
-        // Change the background color when the user scrolls down
         if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+          navbar.style.backgroundColor = '#333';
+          navbar.style.color = '#fff';
           navbar.style.backgroundColor = '#002e6d';
           navbar.style.marginLeft = "-100px";
           navbar.style.height = "140px";
           navbar.style.color = "white";
-          
-
+          changeFontColor('white');
         } else {
           navbar.style.backgroundColor = 'transparent';
+          navbar.style.color = '#002e6d';
+          changeFontColor('inherit');
         }
-
       };
+  
+      function changeFontColor(color) {
+        var links = document.querySelectorAll('#navbar-section a');
+        links.forEach(function(link) {
+          link.style.color = color;
+        });
+      }
