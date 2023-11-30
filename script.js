f7b9d1165707b388f5ae299a6b961422479c86d7
Samenergy <sdushime38@gmail.com>
2023-11-30 10:21:01 +0200

    updated

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
       window.onscroll = function() {
         var navbar = document.getElementById('navbar');
         var links = document.querySelectorAll('#navbar-section a');
+        
+
   
         if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
           navbar.style.backgroundColor = '#333';
@@ -24,3 +26,47 @@
           link.style.color = color;
         });
       }
+
+
+      
+    const apiKey = '2128e8d5509c93e885205d1f1f7bbacd';
+    const city = 'Kigali';
+
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+    // Make the API request
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => {
+        // Handle the API response and update the UI
+        const weatherInfo = document.getElementById('weather-info');
+        const temperatureCelsius = Math.ceil(kelvinToCelsius(data.main.temp));
+
+        weatherInfo.innerHTML = `
+          <p>Kigali</p>
+          <p>T: ${temperatureCelsius} &deg;C</p>
+          <p>W: ${data.weather[0].description}</p>
+         
+        `;
+      })
+      .catch(error => console.error('Error fetching weather data:', error));
+
+    // Function to convert temperature from Kelvin to Celsius
+    function kelvinToCelsius(kelvin) {
+      return kelvin - 273.15;
+    }
+
+     // Change header color on scroll
+     window.addEventListener('scroll', function() {
+      const header = document.getElementById('weather-info');
+      const scrollPosition = window.scrollY;
+
+      // Adjust this value based on when you want the color change to occur
+      const scrollThreshold = 100;
+
+      if (scrollPosition > scrollThreshold) {
+        header.style.color = '#fff'; 
+      } else {
+        header.style.color = '#002e6d'; // Initial text color
+      }
+    });
